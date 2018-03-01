2fccb2efaaa81478ca024ae372275cd676cabdf9
yangjiafu <1737105399@qq.com>
2018-03-01 10:41:13 +0800

    first commit

---
+++ App/models.py
@@ -0,0 +1,69 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class TbMovies(models.Model):
+    m_id = models.AutoField(primary_key=True)
+    m_name = models.CharField(max_length=50)
+    m_othername = models.CharField(max_length=512, blank=True, null=True)
+    m_cover = models.CharField(max_length=100, blank=True, null=True)
+    m_actor = models.CharField(max_length=1024, blank=True, null=True)
+    m_director = models.CharField(max_length=200, blank=True, null=True)
+    m_classify = models.CharField(max_length=40, blank=True, null=True)
+    m_area = models.CharField(max_length=20, blank=True, null=True)
+    m_language = models.CharField(max_length=20, blank=True, null=True)
+    m_releasetime = models.CharField(max_length=30, blank=True, null=True)
+    m_duration = models.CharField(max_length=20, blank=True, null=True)
+    m_score = models.CharField(max_length=10, blank=True, null=True)
+    m_shortcomment = models.CharField(max_length=1024, blank=True, null=True)
+    m_synopsis = models.CharField(max_length=2048, blank=True, null=True)
+    m_linkinfo = models.CharField(db_column='m_linkInfo', max_length=3072)  # Field name made lowercase.
+    m_info = models.CharField(max_length=1024, blank=True, null=True)
+    m_userid = models.CharField(max_length=10, blank=True, null=True)
+    m_plays = models.IntegerField(blank=True, null=True)
+    m_like = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_movies'
+
+
+class TbUsers(models.Model):
+    u_id = models.AutoField(primary_key=True)
+    u_name = models.CharField(max_length=12)
+    u_pwd = models.CharField(max_length=20, blank=True, null=True)
+    u_gender = models.CharField(max_length=3, blank=True, null=True)
+    u_age = models.IntegerField(blank=True, null=True)
+    token = models.CharField(max_length=50, blank=True, null=True)
+    u_vip = models.IntegerField(blank=True, null=True)
+    u_email = models.CharField(max_length=30, blank=True, null=True)
+    u_likemovies = models.CharField(max_length=30, blank=True, null=True)
+    u_duration = models.CharField(max_length=4, blank=True, null=True)
+    u_recording = models.CharField(max_length=30, blank=True, null=True)
+    u_recommendmovie = models.CharField(db_column='u_recommendMovie', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    u_account = models.CharField(unique=True, max_length=10)
+    is_active = models.CharField(max_length=1, blank=True, null=True)
+    u_code = models.CharField(max_length=4, blank=True, null=True)
+    u_ip = models.CharField(max_length=15, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tb_users'
